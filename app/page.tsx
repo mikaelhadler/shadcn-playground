ba2113e410924ee040ec22013e728e54ebd21255
Mikael <mikaelhadler@gmail.com>
2023-12-05 21:22:49 -0300

    include examples

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { CardWithForm } from '@/components/CardWithForm'
+import { TableDemo } from '@/components/TableDemo'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CardWithForm />
-      
+      <TableDemo />
     </main>
   )
 }
